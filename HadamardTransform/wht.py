ffef46586c24b342148a2694e696b14e51ac4d0c
yo <sun4geek@gmail.com>
2018-07-31 15:11:28 +1000

    復調した画像がグレースケールのままだった問題を修正
    復調した画像がグレースケールのままだった問題を修正

--- HadamardTransform/wht.py
+++ HadamardTransform/wht.py
@@ -36,7 +36,7 @@ def sequence(hadamardArray):
 
 def get_ycbcr_array(name: str):
 	pil_img = Image.open('../images/'+name)
-	pil_y, pil_cr, pil_cb = pil_img.convert("YCbCr").split()
+	pil_y, pil_cr, pil_cb = pil_img.convert('YCbCr').split()
 	y = np.asarray(pil_y)
 	cr = np.asarray(pil_cr)
 	cb = np.asarray(pil_cb)
@@ -44,14 +44,23 @@ def get_ycbcr_array(name: str):
 	cr.flags.writeable = True
 	cb.flags.writeable = True
 	return [y, cr, cb]
-	# return np.array([y,cr,cb])
+
+
+def save_ycbcr_as_img(name: str, y, cr, cb):
+	pil_y = Image.fromarray(np.uint8(y))
+	pil_cr = Image.fromarray(np.uint8(cr))
+	pil_cb = Image.fromarray(np.uint8(cb))
+	pil_img = Image.merge('YCbCr', (pil_y, pil_cr, pil_cb)).convert('RGB')
+	pil_img.save('../images/result/' + name)
 
 
 def size_check(length: int, height: int):
+	# 2のべき乗かどうか判断して2のべき乗かつ画像の縦横が等しくなければ終了 等しければ2のN乗になってるかを返す
 	if length != height :
 		print('The image must be square.')
 		sys.exit(1)
-	# (n & (n - 1)) == 0　なら2のべき乗 ->ビット演算で高速化可能
+
+	# (n & (n - 1)) == 0　なら2のべき乗 -> ビット演算で高速化可能
 	while (((length % 2) == 0) and length > 1):
 		length /= 2
 
@@ -59,35 +68,31 @@ def size_check(length: int, height: int):
 		print('Size must be a power of 2.')
 		sys.exit(1)
 
+	return int(math.log(height, 2))
+
 
 def main():
 	imgname = 'lena512.bmp'
 	img = np.array(Image.open('../images/' + imgname), 'f')
-	img_y, img_cb, img_cr = get_ycbcr_array(imgname)
+	img_y, img_cr, img_cb = get_ycbcr_array(imgname)
 	size = len(img)
 
 	# 2のべき乗かつ画像が正方形であるかどうかのチェック
-	size_check(size, len(img[0]))
+	N = size_check(size, len(img[0]))
 
-
-	# N -> 2**N
-	N = int(math.log(size, 2))
 	hadamard = sequence(generate_hadamard(N))
 
-	a = np.dot(hadamard, img_y)
-	G = np.dot(a, hadamard) / 2**N
+	# アダマール変換 G -> 変換係数
+	G_tmp = np.dot(hadamard, img_y)
+	G = np.dot(G_tmp, hadamard) / 2**N
 
-	# G[200:512, 200:512] = 0.0
-	# G[500:512, 500:512] = 100.0
-
-	b = np.dot(hadamard, G)
-	rev = np.dot(b, hadamard) / 2**N
-
-
-	Image.fromarray(np.uint8(rev)).show()
+	# 復調
+	F_tmp = np.dot(hadamard, G)
+	F = np.dot(F_tmp, hadamard) / 2**N
 
 
-
+	Image.fromarray(np.uint8(F)).show()
+	save_ycbcr_as_img(imgname, img_y, img_cr, img_cb)
 
 
 if __name__ == '__main__':
