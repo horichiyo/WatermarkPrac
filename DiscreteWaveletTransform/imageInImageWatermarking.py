55b89b6652407cb168b77fd7e664dbad9d9f7433
yo <sun4geek@gmail.com>
2018-08-08 16:17:22 +1000

    画像表示関数実装，高周波3つそれぞれに違う情報を埋め込んでカラーにするやつ実装
    画像表示関数実装，高周波3つそれぞれに違う情報を埋め込んでカラーにするやつ実装

--- DiscreteWaveletTransform/imageInImageWatermarking.py
+++ DiscreteWaveletTransform/imageInImageWatermarking.py
@@ -10,51 +10,93 @@ wavelet    = 'haar'  # wavelist.txtにどれを指定できるか書いてある
 level      = 1
 
 
-def DWT(coverImageName, watermarkImageName):
+def DWT_gray(coverImageName, watermarkImageName):
     coverImage = np.array(Image.open(imgPath+coverImageName), 'f')
-    # coverImage = cv2.resize(coverImage, (512, 512))
     watermarkImage = np.array(Image.open(imgPath+watermarkImageName), 'f')
     watermarkImage = cv2.resize(watermarkImage, (int(len(coverImage)/2), int(len(coverImage)/2)))
 
-
-
-    # cv2.imshow('Cover Image', cv2.imread(imgPath+coverImageName))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    #
-    # cv2.imshow('Watermark Image', cv2.imread(imgPath+watermarkImageName))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    # _show(cv2.imread(imgPath+coverImageName), title='Cover Image')
+    # _show(cv2.imread(imgPath+watermarkImageName), title='Watermark Image')
 
     # DWT on cover image
     coverImage = np.float64(coverImage)
     coverImage /= 255
-    coeffC = pywt.dwt2(coverImage, 'haar')
+    coeffC = pywt.dwt2(coverImage, wavelet)
     cA, (cH, cV, cD) = coeffC
 
+    # TODO:上のを使って読み込むやつ作成
     _, _, watermarkImage, _, _ = dwt.getImgSizeAndData(watermarkImageName)
     watermarkImage = watermarkImage.astype(np.float64)
     watermarkImage /= 255
 
     # Embedding
-    coeffW = cA, (cH, cV, 1.0*cD + 0.1*watermarkImage)
+    coeffW = cA, (cH, cV, 0.4*cD+0.1*watermarkImage)
 
-    watermarkedImage = pywt.idwt2(coeffW, 'haar')
-    cv2.imshow('Watermarked Image', watermarkedImage)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    watermarkedImage = pywt.idwt2(coeffW, wavelet)
+    _show(watermarkedImage, title='Watermarked Image')
 
     # Extraction
-    coeffWM = pywt.dwt2(watermarkedImage, 'haar')
+    coeffWM = pywt.dwt2(watermarkedImage, wavelet)
     hA, (hH, hV, hD) = coeffWM
 
-    extracted = (hD - 1.0 * cD) / 0.1
-    extracted *= 255;
+    extracted = (hD - 0.4*cD) / 0.1
+    extracted *= 255
     extracted = np.uint8(extracted)
 
-    cv2.imshow('Extracted', extracted)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    _show(extracted, title='Extracted')
+
+
+def DWT_color(coverImageName, watermarkImageName):
+    coverImage = np.array(Image.open(imgPath+coverImageName), 'f')
+    watermarkImage = np.array(Image.open(imgPath+watermarkImageName), 'f')
+    watermarkImage = cv2.resize(watermarkImage, (int(len(coverImage)/2), int(len(coverImage)/2)))
+
+    # _show(cv2.imread(imgPath+coverImageName), title='Cover Image')
+    # _show(cv2.imread(imgPath+watermarkImageName), title='Watermark Image')
+
+    # DWT on cover image
+    coverImage = np.float64(coverImage)
+    coverImage /= 255
+    coeffC = pywt.dwt2(coverImage, wavelet)
+    cA, (cH, cV, cD) = coeffC
+
+    # TODO:上のを使って読み込むやつ作成
+    _, _, watermarkImage, cr, cb = dwt.getImgSizeAndData(watermarkImageName)
+    watermarkImage = watermarkImage.astype(np.float64)
+    watermarkImage /= 255
+
+    cr = cr.astype(np.float64)
+    cr /= 255
+    cb = cb.astype(np.float64)
+    cb /= 255
+
+    # Embedding
+    # coeffW = cA, (cH, cV, 0.4*cD+0.1*watermarkImage)
+    coeffW = cA, (cH*0.4+0.1*cr, cV*0.4+0.1*cb, 0.4*cD+0.1*watermarkImage)
+
+    watermarkedImage = pywt.idwt2(coeffW, wavelet)
+    _show(watermarkedImage, title='Watermarked Image')
+
+    # Extraction
+    coeffWM = pywt.dwt2(watermarkedImage, wavelet)
+    hA, (hH, hV, hD) = coeffWM
+
+    # extracted = (hD - 0.4*cD) / 0.1
+    # extracted *= 255
+    # extracted = np.uint8(extracted)
+    #
+    # _show(extracted, title='Extracted')
+
+    y = (hD - 0.4*cD) / 0.1
+    y *= 255
+    y = np.uint8(y)
+    b = (hV - 0.4*cV) / 0.1
+    b*=255
+    b = np.uint8(b)
+    r = (hH - 0.4*cV) / 0.1
+    r*=255
+    r = np.uint8(r)
+    dwt.saveYcbcrAsImg('lake_out.bmp', y, r, b)
 
 
 def _saveGrayImg(imgName):
@@ -62,9 +104,14 @@ def _saveGrayImg(imgName):
     gray_img = Image.fromarray(np.uint8(img)).convert('L')
     gray_img.save(outImgPath+'gray'+imgName)
 
+def _show(img,title='title'):
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 def main():
-    DWT('graylena512.bmp', 'lake.bmp')
+    DWT_gray('graylena512.bmp', 'lake.bmp')
 
 if __name__ == '__main__':
     # https://goo.gl/forms/D0ioYIAt0gpwYyGO2
