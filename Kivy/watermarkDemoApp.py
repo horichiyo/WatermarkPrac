7f90793ae022c0c664c3dfa4ce9df3649c3b92e2
yo <sun4geek@gmail.com>
2018-08-18 00:02:44 +1000

    文字の埋め込みと抽出ができるように実装
    埋め込みと抽出ができるように実装

--- Kivy/watermarkDemoApp.py
+++ Kivy/watermarkDemoApp.py
@@ -13,24 +13,33 @@ LabelBase.register(DEFAULT_FONT, 'ipaexg.ttf')
 import sys
 sys.path.append('..')
 from Tools import makeqr
+from watermarking import imageInImageWatermarking
+
 
 class TestWidget(Widget):
     text = StringProperty()
     image_src =StringProperty()
+    embed_message = StringProperty()
+    extract_message = StringProperty()
+
 
     def __init__(self, **kwargs):
         super(TestWidget, self).__init__(**kwargs)
         self.text = 'This is Wartermarking Demo App.ああ'
         self.image_src = '../Images/lena256.bmp'
 
-    def buttonClicked(self):
+    def embedButtonClicked(self):
         self.text = 'Embed'
+        self.embed_message =  self.ids["text_box"].text
+        imageInImageWatermarking.embedQrcodeUseFFT(self.embed_message)
 
     def buttonClicked2(self):
         self.text = 'Show'
 
     def buttonClicked3(self):
         self.text = 'Extract'
+        extract_message = imageInImageWatermarking.decodeQrcode('fft')
+        print(extract_message)
 
     def descriptionButtonClicked(self):
         descriptionView = DescriptionView()
